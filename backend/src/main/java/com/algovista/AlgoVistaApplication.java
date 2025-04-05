a1e0cf29bd08f3cc31b9fa7b52b5dbcf632da580
Tirthoraj Bhattacharya <tirthorajbhattacharya@gmail.com>
2025-04-05 20:34:30 +0530

    initial commit

---
+++ backend/src/main/java/com/algovista/AlgoVistaApplication.java
@@ -0,0 +1,11 @@
+package com.algovista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoVistaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoVistaApplication.class, args);
+    }
+}
